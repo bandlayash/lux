a42b9d9331616e7709523585a2e994604e388bc4
ybandla <ybandla@uiowa.edu>
2024-11-08 16:38:53 -0600

    Added frame counter and test file

--- lux.py
+++ lux.py
@@ -8,6 +8,7 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+fps_counter = pygame.time.Clock.get_fps
 
 while running:
     #quit window
@@ -17,6 +18,13 @@ while running:
     screen.fill("purple")
     pygame.draw.circle(screen, "blue", player_pos, 40)
     keys = pygame.key.get_pressed()
+
+    #fps counter
+    fps = fps_counter(clock)
+    font = pygame.font.Font("C:\Windows\Fonts\SHOWG.ttf", 30)
+    fps_text = font.render(f"FPS: {fps:.2f}", True, (255, 255, 255))
+    screen.blit(fps_text, (10, 10))
+    
 
     # WASD functionality
     if keys[pygame.K_w]:
@@ -28,5 +36,5 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
     pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(120) / 1000
 pygame.quit()
