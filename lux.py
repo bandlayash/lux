c1739a2ad818a60b020aa0db27af871ec75e0318
ybandla <ybandla@uiowa.edu>
2024-11-08 16:10:09 -0600

    pygame setup test

--- lux.py
+++ lux.py
@@ -1,1 +1,17 @@
+#imports
 import pygame
+
+#initialize pygame
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+   for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+         running = False
+   screen.fill("purple")
+   pygame.display.flip()
+   clock.tick(60)
+pygame.quit()
