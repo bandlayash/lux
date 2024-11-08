6d0bb823a16eac2425a7383201378de50f1086e3
ybandla <ybandla@uiowa.edu>
2024-11-08 16:21:00 -0600

    N/A

--- lux.py
+++ lux.py
@@ -10,21 +10,23 @@ dt = 0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
-   #quit window
-   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-         running = False
-   screen.fill("purple")
-   pygame.draw.circle(screen, "red", player_pos, 40)
-   keys = pygame.key.get_pressed()
-   if keys[pygame.K_w]:
+    #quit window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill("purple")
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    keys = pygame.key.get_pressed()
+
+
+    if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
-   if keys[pygame.K_s]:
+    if keys[pygame.K_s]:
         player_pos.y += 300 * dt
-   if keys[pygame.K_a]:
+    if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-   if keys[pygame.K_d]:
+    if keys[pygame.K_d]:
         player_pos.x += 300 * dt
-   pygame.display.flip()
-   dt = clock.tick(60) / 1000
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
 pygame.quit()
